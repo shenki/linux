3127a6b45c9dbda7784eb35c90e9ce0688172a15
Joel Stanley <joel@jms.id.au>
2016-10-19 14:48:38 +1030

    Aspeed SDK v00.03.23
    Taken from ast_sdk.v00.03.23
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -13,6 +13,9 @@
 #include <linux/pci-aspm.h>
 #include <asm-generic/pci-bridge.h>
 #include "pci.h"
+#ifdef CONFIG_PLAT_ASPEED
+#include <mach/irqs.h>
+#endif
 
 #define CARDBUS_LATENCY_TIMER	176	/* secondary latency timer */
 #define CARDBUS_RESERVE_BUSNR	3
@@ -975,7 +978,11 @@ static void pci_read_irq(struct pci_dev *dev)
 	dev->pin = irq;
 	if (irq)
 		pci_read_config_byte(dev, PCI_INTERRUPT_LINE, &irq);
+#ifdef CONFIG_PLAT_ASPEED
+	dev->irq = dev->pin + IRQ_PCIE_CHAIN_START - 1;
+#else
 	dev->irq = irq;
+#endif
 }
 
 void set_pcie_port_type(struct pci_dev *pdev)
