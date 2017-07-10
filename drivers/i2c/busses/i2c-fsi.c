f1bbec7a68f19d4e2f07fe43b87ec2ae5849b80a
Edward A. James <eajames@us.ibm.com>
2017-07-10 13:38:00 -0500

    i2c: fsi: Add I2C master locking to FSI algorithm
    Since there are many ports per master, each with it's own adapter and
    chardev, we need some locking to prevent transfers from changing the\
    master state while other transfers are in progress.
    
    Signed-off-by: Edward A. James <eajames@us.ibm.com>
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/i2c/busses/i2c-fsi.c
+++ drivers/i2c/busses/i2c-fsi.c
@@ -19,6 +19,8 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/sched.h>
+#include <linux/semaphore.h>
+#include <linux/wait.h>
 
 #define FSI_ENGID_I2C		0x7
 
@@ -135,6 +137,8 @@ struct fsi_i2c_master {
 	struct fsi_device	*fsi;
 	u8			fifo_size;
 	struct list_head	ports;
+	wait_queue_head_t	wait;
+	struct semaphore	lock;
 };
 
 struct fsi_i2c_port {
@@ -166,6 +170,29 @@ static int fsi_i2c_write_reg(struct fsi_device *fsi, unsigned int reg,
 	__be32 data_be = cpu_to_be32(*data);
 
 	return fsi_device_write(fsi, reg, &data_be, sizeof(data_be));
+}
+
+static int fsi_i2c_lock_master(struct fsi_i2c_master *i2c, int timeout)
+{
+	int rc;
+
+	rc = down_trylock(&i2c->lock);
+	if (!rc)
+		return 0;
+
+	rc = wait_event_interruptible_timeout(i2c->wait,
+					      !down_trylock(&i2c->lock),
+					      timeout);
+	if (rc > 0)
+		return 0;
+
+	return -EBUSY;
+}
+
+static void fsi_i2c_unlock_master(struct fsi_i2c_master *i2c)
+{
+	up(&i2c->lock);
+	wake_up(&i2c->wait);
 }
 
 static int fsi_i2c_dev_init(struct fsi_i2c_master *i2c)
@@ -409,9 +436,13 @@ static int fsi_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg *msgs,
 	struct fsi_i2c_port *port = adap->algo_data;
 	struct i2c_msg *msg;
 
-	rc = fsi_i2c_set_port(port);
+	rc = fsi_i2c_lock_master(port->master, adap->timeout);
 	if (rc)
 		return rc;
+
+	rc = fsi_i2c_set_port(port);
+	if (rc)
+		goto unlock;
 
 	for (i = 0; i < num; ++i) {
 		msg = msgs + i;
@@ -419,15 +450,17 @@ static int fsi_i2c_xfer(struct i2c_adapter *adap, struct i2c_msg *msgs,
 
 		rc = fsi_i2c_start(port, msg, i == num - 1);
 		if (rc)
-			return rc;
+			goto unlock;
 
 		rc = fsi_i2c_wait(port, msg,
 				  adap->timeout - (jiffies - start_time));
 		if (rc)
-			return rc;
+			goto unlock;
 	}
 
-	return 0;
+unlock:
+	fsi_i2c_unlock_master(port->master);
+	return rc;
 }
 
 static u32 fsi_i2c_functionality(struct i2c_adapter *adap)
@@ -452,6 +485,8 @@ static int fsi_i2c_probe(struct device *dev)
 	if (!i2c)
 		return -ENOMEM;
 
+	init_waitqueue_head(&i2c->wait);
+	sema_init(&i2c->lock, 1);
 	i2c->fsi = to_fsi_dev(dev);
 	INIT_LIST_HEAD(&i2c->ports);
 
