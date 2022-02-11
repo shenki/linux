b73af097f0294073ec7b1c22208d15e11911775b
Jeremy Kerr <jk@codeconstruct.com.au>
2022-02-11 09:15:52 +0800

    mctp: serial: Cancel pending work from ndo_uninit handler
    We cannot do the cancel_work_sync from after the unregister_netdev, as
    the dev pointer is no longer valid, causing a uaf on ldisc unregister
    (or device close).
    
    Instead, do the cancel_work_sync from the ndo_uninit op, where the dev
    still exists, but the queue has stopped.
    
    OpenBMC-Staging-Count: 1
    Fixes: 7bd9890f3d74 ("mctp: serial: cancel tx work on ldisc close")
    Reported-by: Luo Likang <luolikang@nsfocus.com>
    Tested-by: Luo Likang <luolikang@nsfocus.com>
    Signed-off-by: Jeremy Kerr <jk@codeconstruct.com.au>
    Link: https://lore.kernel.org/r/20220211011552.1861886-1-jk@codeconstruct.com.au
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    (cherry picked from commit 6c342ce2239c182c2428ce5a44cb32330434ae6e)
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/net/mctp/mctp-serial.c
+++ drivers/net/mctp/mctp-serial.c
@@ -491,7 +491,6 @@ static void mctp_serial_close(struct tty_struct *tty)
 	int idx = dev->idx;
 
 	unregister_netdev(dev->netdev);
-	cancel_work_sync(&dev->tx_work);
 	ida_free(&mctp_serial_ida, idx);
 }
 
